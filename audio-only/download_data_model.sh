65c8148bc2cdfd7234b46f28eab2ec4929fa71fb
ChiSheng Yang <chisheny@umich.edu>
2020-04-25 18:14:17 -0400

    initial commit

---
+++ audio-only/download_data_model.sh
@@ -0,0 +1,13 @@
+#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=17sZxLu9wdbHKBg6bn8-AAqhevhCTF16S' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=17sZxLu9wdbHKBg6bn8-AAqhevhCTF16S" -O ao_only.zip && rm -rf /tmp/cookies.txt
+
+unzip ao_only.zip
+
+rm -rf __MACOSX
+mkdir model
+mkdir ../data
+mkdir results
+mv unet_data/UNetModel_set1000_1.pth ./model/
+mv unet_data/translator.mp4 ../data/
+rm -rf unet_data
+rm ao_only.zip
+
