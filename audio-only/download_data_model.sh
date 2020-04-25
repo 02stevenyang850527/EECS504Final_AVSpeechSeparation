d0622df1f2aa628717dc65b60680dc04936bd31b
Chi-Sheng Yang <chisheny@umich.edu>
2020-04-25 18:41:54 -0400

    uncomment wget

--- audio-only/download_data_model.sh
+++ audio-only/download_data_model.sh
@@ -1,4 +1,4 @@
-#wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=17sZxLu9wdbHKBg6bn8-AAqhevhCTF16S' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=17sZxLu9wdbHKBg6bn8-AAqhevhCTF16S" -O ao_only.zip && rm -rf /tmp/cookies.txt
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=17sZxLu9wdbHKBg6bn8-AAqhevhCTF16S' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=17sZxLu9wdbHKBg6bn8-AAqhevhCTF16S" -O ao_only.zip && rm -rf /tmp/cookies.txt
 
 unzip ao_only.zip
 
